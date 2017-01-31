1decc2df100afb18cddb1c994099b62be69aa690
yuvraj0042 <yuvrajsingh0042@gmail.com>
2017-01-31 06:35:00 +0000

    third commit

---
+++ 01.py
@@ -0,0 +1,11 @@
+per =input("enter your precentage")
+if per>=80:
+ print"first division honors"
+elif per>=60:
+ print"first division"
+elif per>=50:
+ print"second division"
+elif per>=40:
+ print"third division"
+else:
+ print"fail"
