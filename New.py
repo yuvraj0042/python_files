e670f349d49a00b7d05bd6d1b97b31811b439005
yuvraj0042 <yuvrajsingh0042@gmail.com>
2017-02-02 04:57:00 +0000

    final

--- New.py
+++ New.py
@@ -1,6 +1,8 @@
 import math
-a=input("enter")
-b=input("enter")
-c=input("enter")
-x=(-(b)+math.sqrt(b*b-4*a*c))/(2*a)
-print x
+print ("enter the value of factorise")
+a=input("enter first digit")
+b=input("enter second digit")
+c=input("enter third digit")
+x1=(-(b)+math.sqrt(b*b-4*a*c))/(2*a)
+x2=(-(b)-math.sqrt(b*b-4*a*c))/(2*a)
+print "the value of positivce x",x1, "the value of negative x",x2
